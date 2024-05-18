ffb53c6adc440d9b1acdfc26b8cca840c2f1662e
Sarah Abbad <sarah.abbad04@gmail.com>
2024-05-18 20:42:57 +0100

    photos adjusted + about section

---
+++ js/script.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function () {
+    window.onscroll = function () {
+      scrollFunction();
+    };
+  
+    function scrollFunction() {
+      if (
+        document.body.scrollTop > 20 ||
+        document.documentElement.scrollTop > 20
+      ) {
+        document.getElementById("scrollToTopBtn").style.display = "block";
+      } else {
+        document.getElementById("scrollToTopBtn").style.display = "none";
+      }
+    }
+  });
+  
