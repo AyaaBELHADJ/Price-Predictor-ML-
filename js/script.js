4241879a2007631856d790b0f274dd165e7045f6
Sarah Abbad <sarah.abbad04@gmail.com>
2024-05-20 02:05:23 +0100

    contact's sending confirmation

--- js/script.js
+++ js/script.js
@@ -1,17 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
-    window.onscroll = function () {
-      scrollFunction();
-    };
-  
-    function scrollFunction() {
-      if (
-        document.body.scrollTop > 20 ||
-        document.documentElement.scrollTop > 20
-      ) {
-        document.getElementById("scrollToTopBtn").style.display = "block";
-      } else {
-        document.getElementById("scrollToTopBtn").style.display = "none";
-      }
+  window.onscroll = function () {
+    scrollFunction();
+  };
+
+  function scrollFunction() {
+    if (
+      document.body.scrollTop > 20 ||
+      document.documentElement.scrollTop > 20
+    ) {
+      document.getElementById("scrollToTopBtn").style.display = "block";
+    } else {
+      document.getElementById("scrollToTopBtn").style.display = "none";
     }
-  });
+  }
+});
+
+
+document.getElementById('contactForm').addEventListener('submit', function(event) {
+  event.preventDefault(); 
   
+  var successModal = new bootstrap.Modal(document.getElementById('successModal'));
+  successModal.show();
+ 
+});
